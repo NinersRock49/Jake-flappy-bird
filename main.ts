38bb4afb260b862929e2a4a48b496aa947d18639
Aditya Nair <nair.aditya.r@gmail.com>
2020-06-17 13:53:48 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+
 scene.setBackgroundColor(9)
 
 let bird = sprites.create(img`
@@ -30,16 +31,81 @@ game.onUpdate(function () {
         game.over()
     }
 
-if (bird.y() > scene.screenHeight()) {
-    game.over()
-}
+    if (bird.y() > scene.screenHeight()) {
+        game.over()
+    }
 
 })
 
+info.setScore(0)
+
+game.onUpdateInterval(1000, function () {
+    info.changeScoreBy(1)
+})
 
 
+function spawnTopTree() {
+    let obstacleTop = sprites.create(img`
+        . . . . . 6 f c e e e e e e e e e e 6 . . . . .
+        . . . . 6 7 7 6 e e e e e e e e e 6 7 6 . . . .
+        . . . 6 7 7 7 6 6 6 e e e e 6 6 6 6 7 7 6 . . .
+        . . 6 7 7 6 8 e 6 7 7 6 6 7 7 7 6 6 7 7 7 6 . .
+        . . . 6 6 8 e e 7 7 6 8 8 6 7 7 8 8 6 6 6 . . .
+        . . . . . . e e 7 7 e e e e 7 7 e c e e . . . .
+        . . . . . . e e 6 e e e e e e 6 e e f . . . . .
+        . . . . . . e e e e e e e e e e e e f . . . . .
+        . . . . . . e e e e e e e e e e e e f . . . . .
+        . . . . . . e e e e e e e e e e e c f . . . . .
+        . . . . . . c e e e e e e e e e e c f . . . . .
+        . . . . . . c e e e e e e e e e e f f . . . . .
+        . . . . . . f e e e e e e e e e e f e . . . . .
+        . . . . . 6 f e e e e e e e e e e f 6 . . . . .
+        . . . . 6 7 7 6 e e e e e e e e e 6 7 6 . . . .
+        . . . 6 7 7 7 6 6 6 e e e e 6 6 6 7 7 7 6 . . .
+        . . 6 7 7 6 e e 6 7 7 7 7 7 7 7 6 6 7 7 7 6 . .
+        . . . 6 6 8 e e 7 7 6 8 8 6 7 7 8 8 6 6 6 . . .
+        . . . . . . e e 7 7 e e e e 6 7 e e e e . . . .
+        . . . . . . e e 6 e e e e e e 6 c e f . . . . .
+        . . . . . . e e e e e e e e e e e e f . . . . .
+        . . . . . . e e e e e e e e e e e e f . . . . .
+        . . . . . . e e e e e e e e e e e c f . . . . .
+        . . . . . . e e e e e e e e e e e c f . . . . .
+        . . . . . . e e e e e e e e e e e f f . . . . .
+        . . . . . . f e e e e e e e e e e f e . . . . .
+        . . . . . 6 f e e e e e e e e e e f 6 . . . . .
+        . . . . 6 7 7 6 e e e e e e e e e 6 7 6 . . . .
+        . . . 6 7 7 7 6 6 6 e e e e 6 6 6 7 7 7 6 . . .
+        . . 6 7 7 6 e e 6 7 7 7 7 7 7 7 6 6 7 7 7 6 . .
+        . . . 6 6 8 e e 7 7 6 8 8 6 7 7 8 8 6 6 6 . . .
+        . . . . . . e e 7 7 e e e e 6 7 e e . . . . . .
+        . . . . . . e e 6 e e e e e e 6 c e . . . . . .
+        . . . . . . e e f e e e e e e e c e . . . . . .
+        . . . . . . e e c e e e e e e e c e . . . . . .
+        . . . . . . e e c e e e e e e e f e . . . . . .
+        . . . . . . e e c e e e e e e e f e . . . . . .
+        . . . . . . e e e e e e e e e e f e . . . . . .
+        . . . . . . e e e e e e e e e e c e . . . . . .
+        . . . . . 6 e e e e e e e e e e c e 6 . . . . .
+        . . . . 6 7 7 6 e e e e e e e e e 6 7 6 . . . .
+        . . . 6 7 7 6 6 6 6 e e e e 6 7 6 6 7 7 6 . . .
+        . . 6 7 7 6 e e 7 7 7 7 7 7 7 7 6 6 7 7 7 6 . .
+        . . . 6 6 8 c e 7 7 6 8 8 6 7 7 8 8 6 6 6 . . .
+        . . . . . . c e 7 7 e e e e 6 7 e e . . . . . .
+        . . . . . . e e e e e e e e e e e e . . . . . .
+        . . . . . . e e e e e e e e e e e e . . . . . .
+        . . . . . . e e e e e e e e e e e e . . . . . .
+        . . . . . . e e e e e e e e e e e e . . . . . .
+        . . . . . . e e e e e e e e e e e e . . . . . .
+        . . . . . . e e e e e e e e e e e e . . . . . .
+        . . . . . . e e e e e e e e e e e e . . . . . .
+        . . . . . . b e e e e e e e e e e b . . . . . .
+        . . . . . . . b e e e e e e e e b . . . . . . .
+        . . . . . . . . b e e e e e e b . . . . . . . .
+        . . . . . . . . . . . . . . . . . . . . . . . .
+    `)
 
+    obstacleTop.setPosition(scene.screenWidth(), 25)
+    obstacleTop.vx = -25
+}
 
-
-
-
+spawnTopTree()
